6eec2352022cf39656e9c6904c24c3a13014412b
prorocky <oshadian@ucsc.edu>
2022-07-16 15:26:16 -0700

    Obstacle generation working, able to change via variables in game manager

--- Assets/Scripts/FloorTile.cs
+++ Assets/Scripts/FloorTile.cs
@@ -6,17 +6,23 @@ public class FloorTile : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private LayerMask playerLayer;
+    [SerializeField] public GameManager gameManager;
     
     private FloorSpawner floorSpawner;
     void Awake()
     {
         floorSpawner = GameObject.FindObjectOfType<FloorSpawner>();
+        gameManager = GameObject.FindObjectOfType<GameManager>();
     }
 
     private void OnTriggerEnter(Collider col) 
     {   
         if (col.gameObject.layer == 6) {
-            floorSpawner.SpawnTile();
+            if (gameManager.tileSpawnNo % gameManager.obstacleSpawnFrequency == 0) {
+                floorSpawner.SpawnTile();
+            } else {
+                floorSpawner.SpawnTileNoObstacle();
+            }
             Destroy(gameObject, 3);
         }
     }
--- Assets/Scripts/boofMovement.cs
+++ Assets/Scripts/boofMovement.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class boofMovement : MonoBehaviour
 {
+    public float moveSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        moveSpeed = -0.5f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position - new Vector3(0, 0, -0.02f);
+        transform.position = transform.position - new Vector3(0, 0, moveSpeed);
     }
 }
