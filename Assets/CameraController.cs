4e208dbec5b705378b0b6647269200a84444a064
Dylan Soungpanya <dsoungpa@ucsc.edu>
2022-07-16 15:58:52 -0700

    Added Lane Swapping and Dice Rolling

---
+++ Assets/CameraController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Header("General")]
+    [SerializeField] private float movementSpeed = 1f;
+    [SerializeField] private Transform target;
+    [SerializeField] private float smoothSpeed;
+    private Vector3 targetPosition;
+
+    void Start(){
+        transform.position = new Vector3(target.position.x, target.position.y + 2f, target.position.z - 5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPos = new Vector3(target.position.x, target.position.y + 2f, target.position.z - 5f);
+        transform.position = Vector3.Lerp(transform.position, targetPos, smoothSpeed);
+
+        targetPosition = transform.position - new Vector3(0,0, -100f);
+        var step = movementSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
+        
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,145 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("General")]
+    [SerializeField] private float movementSpeed = 1f;
+    [SerializeField] private float switchLaneSpeed = 1f;
+    [SerializeField] private float rotateDuration = 1f;
+     [SerializeField] private float rotationSpeed = 10f;
+    [SerializeField] private float leftLaneXPosition;
+    [SerializeField] private float middleLaneXPosition;
+    [SerializeField] private float rightLaneXPosition;
+   
+
+    private Vector3 targetPosition;
+    private bool rotating = false;
+    private Quaternion newRotation;
+    private Quaternion oldRotation;
+    
+    
+
+
+    //[Header("Reference")]
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        newRotation = oldRotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Roll Dice
+        UpdateRotationState();
+
+        if(rotateDuration < 1f){
+            rotateDuration += Time.deltaTime * rotationSpeed;
+            transform.rotation = Quaternion.Slerp(oldRotation, newRotation, rotateDuration);
+        }
+
+        // Constantly Move Foward
+        targetPosition = transform.position - new Vector3(0,0, -100f);
+        var moveZStep = movementSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveZStep);
+
+        // Switch to Left Lane
+        if(Input.GetKeyDown("q")){
+            // In middle lane, move left
+            if(transform.position.x == middleLaneXPosition){
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(leftLaneXPosition, transform.position.y, transform.position.z), switchLaneSpeed);
+            }
+
+            // In right lane, move left
+            if(transform.position.x == rightLaneXPosition){
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(middleLaneXPosition, transform.position.y, transform.position.z), switchLaneSpeed);
+            }          
+        }
+
+        // Switch to Right Lane
+        if(Input.GetKeyDown("e")){
+            // In middle lane, move right
+            if(transform.position.x == middleLaneXPosition){
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(rightLaneXPosition, transform.position.y, transform.position.z), switchLaneSpeed);
+            }
+
+            // In left lane, move rightc
+            if(transform.position.x == leftLaneXPosition){
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(middleLaneXPosition, transform.position.y, transform.position.z), switchLaneSpeed);
+            }          
+        }
+
+        // // Rotate Upwards
+        // if(Input.GetKeyDown("w") && !rotating){
+        //     StartCoroutine(Rotate(Quaternion.Euler(90, 0, 0)));
+        // }
+
+        // // Rotate Downwards
+        // if(Input.GetKeyDown("s") && !rotating){
+        //     StartCoroutine(Rotate(Quaternion.Euler(-90, 0, 0)));
+        // }
+
+        // // Rotate Left
+        // if(Input.GetKeyDown("a") && !rotating){
+        //     StartCoroutine(Rotate(Quaternion.Euler(0, 90, 0)));
+        // }
+
+        // // Rotate Right
+        // if(Input.GetKeyDown("d") && !rotating){
+        //     StartCoroutine(Rotate(Quaternion.Euler(0, -90, 0)));
+        // }
+
+    }
+
+    void UpdateRotationState(){
+        // Get the rotation, if any
+        float x = 0f, y = 0f;
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            print("Forward");
+            x = 90f;
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            print("Back");
+            x = -90f;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            print("Left");
+            y = 90f;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            print("Right");
+            y = -90f;
+        }
+        // if rotation is nonzero, apply it
+        if (x != 0f || y != 0f)
+        {
+            newRotation = Quaternion.Euler(x, y, 0f) * newRotation;
+            oldRotation = transform.rotation;
+            rotateDuration = 0f;
+        }
+    }
+
+    IEnumerator Rotate(Quaternion shift){
+        rotating = true;
+        float timeElapsed = 0;
+        Quaternion startRotation = transform.rotation;
+        Quaternion targetRotation = transform.rotation * shift;
+        print("TARGET: " + targetRotation.x);
+
+        while(timeElapsed < rotateDuration){
+            transform.rotation = Quaternion.Slerp(startRotation, targetRotation, timeElapsed / rotateDuration );
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.rotation = targetRotation;
+        rotating = false;
+    }
+}
