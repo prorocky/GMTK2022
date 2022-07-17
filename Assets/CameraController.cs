f6a5d9d1c4bf3d8a36cf652cdedbd76cb6317388
Vhergo <btessema@ucsc.edu>
2022-07-16 18:00:15 -0700

    messing around with player legs

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -8,16 +8,17 @@ public class CameraController : MonoBehaviour
     [SerializeField] private float movementSpeed = 1f;
     [SerializeField] private Transform target;
     [SerializeField] private float smoothSpeed;
+    [SerializeField] private float[] cameraOffset = new float[] {2f, 5f};
     private Vector3 targetPosition;
 
     void Start(){
-        transform.position = new Vector3(target.position.x, target.position.y + 2f, target.position.z - 5f);
+        transform.position = new Vector3(target.position.x, target.position.y + cameraOffset[0], target.position.z - cameraOffset[1]);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 targetPos = new Vector3(target.position.x, target.position.y + 2f, target.position.z - 5f);
+        Vector3 targetPos = new Vector3(target.position.x, target.position.y + cameraOffset[0], target.position.z - cameraOffset[1]);
         transform.position = Vector3.Lerp(transform.position, targetPos, smoothSpeed);
 
         targetPosition = transform.position - new Vector3(0,0, -100f);
