a054cdf8934304412d99d1e8a6c2396c8b344a33
prorocky <oshadian@ucsc.edu>
2022-07-17 00:28:46 -0700

    changed camera angle

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -8,7 +8,7 @@ public class CameraController : MonoBehaviour
     [SerializeField] private float movementSpeed = 1f;
     [SerializeField] private Transform target;
     [SerializeField] private float smoothSpeed;
-    [SerializeField] private float[] cameraOffset = new float[] {2f, 5f};
+    [SerializeField] private float[] cameraOffset = new float[] {10f, 5f};
     private Vector3 targetPosition;
 
     void Start(){
@@ -21,7 +21,7 @@ public class CameraController : MonoBehaviour
         Vector3 targetPos = new Vector3(target.position.x, target.position.y + cameraOffset[0], target.position.z - cameraOffset[1]);
         transform.position = Vector3.Lerp(transform.position, targetPos, smoothSpeed);
 
-        targetPosition = transform.position - new Vector3(0,0, -100f);
+        targetPosition = transform.position - new Vector3(0, 0, -100f);
         var step = movementSpeed * Time.deltaTime;
         transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
         
